c9efa24f9505a43e753451905da832fe3eae24e7
gmayankcse15 <gmayank386.cse15@nituk.ac.in>
2017-08-06 15:39:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,7 +13,7 @@ var marginLeft = 0 ;
  
 img.onclick = function() {
  setInterval(function()
- {    
+ {    console.log('Implemented') ;
      marginLeft = marginLeft + 10 ;
     img.style.marginLet = marginLeft + 'px' ;
  }, 3000);
