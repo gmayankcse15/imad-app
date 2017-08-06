854380684a581b347345e424a8f83c909232f973
gmayankcse15 <gmayank386.cse15@nituk.ac.in>
2017-08-06 13:09:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,18 @@ element.innerHTML = 'New value' ;
 
 
 // Move the image
-var img = document.getElementById('madi');
+/*var img = document.getElementById('madi');
 img.onclick = function() {
   img.style.marginLeft = '100px' ;  
 };
+*/
+
+var img = document.getElementById('madi');
+var marginLeft = 0 ;
+function moveRight() {
+    marginLeft = marginLeft +1 ;
+    img.style.marginLet = marginLeft + 'px' ;
+}
+img.onclick = function() {
+    var interval = setInterval(moveRight, 50) ;
+}
