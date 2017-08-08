88e7f622050077dcd3a828dc83fb76261c6c1d33
gmayankcse15 <gmayank386.cse15@nituk.ac.in>
2017-08-08 19:27:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -65,10 +65,10 @@ button.onclick = function() {
         
     };
     var nameInput = document.getElementById('name');
-var name = nameInput.value ;
+     var name = nameInput.value ;
     
     // Make the request
-    request.open('GET', 'http://gmayank386cse15.imad.hasura-app.io/counter',true) ;
+    request.open('GET', 'http://gmayank386cse15.imad.hasura-app.io/submit-name?name='+ name, true) ;
     request.send(null) ;
 
     
