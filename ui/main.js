8f36f5e538282ecc2cf842d5cd2aad115c568618
gmayankcse15 <gmayank386.cse15@nituk.ac.in>
2017-08-08 16:43:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -42,7 +42,8 @@ button.onclick = function() {
     request.onreadstatechange = function() {
         
     if(request.readyState === XMLHttpRequest.DONE) {
-        if(request.status === 200){
+        if(request.status === 200)
+          {
             var counter = request.responseText;
             var span = document.getElementById('count');
             span.innerHTML = counter.toString() ;
