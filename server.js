f9761f1cc0194cb8add69e630f2426a4abe41005
gmayankcse15 <gmayank386.cse15@nituk.ac.in>
2017-08-08 16:14:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,16 +101,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0 ;
+app.get('/counter', function(req, res){
+    
+ res.sendFile(counter.toString());
+});
+
+
 app.get('/:articleName', function(req, res){
    var articleName = req.params.articleName ;
    res.send(createTemplate(articles[articleName])) ;
 });
 
-var counter = 0 ;
-app.get('/counter', function(req, res){
-    
- console.log(counter.toString());
-});
 
 
 
