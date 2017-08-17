55d4763054e2a13b9a476bc7273f8786d84a52e2
gmayankcse15 <gmayank386.cse15@nituk.ac.in>
2017-08-17 20:26:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var http = require('http') ;
+var Pool = require('pg').Pool ;
+
+var config = {
+    user: 'gmayank386cse15',
+    database: 'gmayank386cse15'
+    host: 'http://db.imad.hasura-app.io'
+    port: '5432'
+    password: process.env.DB_PASSWORD
+    
+}
 
 var app = express();
 app.use(morgan('combined'));
@@ -102,6 +113,21 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var pool = new Pool(config);
+app.get('/test-db',function(req, res){
+   //make a select request
+   
+   
+   // return a response with the result
+    
+    pool.query('SELECT * FROM test', function(err, result)){
+        if(err){
+            res.status(500).send(err.toString())
+        }else{
+            res.send(JSON.stringify(result.rows)) ;
+        }
+    }
+});
 var counter = 0 ;
 app.get('/counter', function(req, res){
     counter = counter + 1 ; 
