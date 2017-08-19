42bdc5fd413b5cdaa0e3bc849631c70ad72d81fe
gmayankcse15 <gmayank386.cse15@nituk.ac.in>
2017-08-19 21:55:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -134,7 +134,7 @@ app.get('/hash/:input', function(req, res){
     
 }) ;
 
-app.post('/create-user', function(req, res){
+app.post('/login', function(req, res){
     // username, password 
   // JSON
    var username = req.body.username ;
