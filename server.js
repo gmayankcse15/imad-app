2068903a33076ad80ed1313e0acadc1435c91f5d
gmayankcse15 <gmayank386.cse15@nituk.ac.in>
2017-08-19 18:48:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -113,6 +113,25 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+function hash(input, salt){
+    
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return ["pbkdf2", "10000", salt, hashed.toString('hex')].join($) ;
+    
+}
+
+
+app.get('/hash/:input', function(req, res){
+    
+    var hashedString = hash(req.params.input, 'this-is-some-random-string');
+    res.send(hashedString) ;
+    
+    
+}) ;
+
+
+
 var pool = new Pool(config);
 
 app.get('/test-db',function(req, res){
